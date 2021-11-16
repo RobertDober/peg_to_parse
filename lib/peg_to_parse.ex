e2e7a52e708eaa6ced8879ae88400121bb863361
RobertDober <robert.dober@gmail.com>
2021-11-16 14:57:34 +0100

    README created

--- lib/peg_to_parse.ex
+++ lib/peg_to_parse.ex
@@ -1,18 +1,13 @@
 defmodule PegToParse do
   @moduledoc """
-  Documentation for `PegToParse`.
+
+  [![CI](https://github.com/robertdober/peg_to_parse/workflows/CI/badge.svg)](https://github.com/robertdober/peg_to_parse/actions)
+  [![Coverage Status](https://coveralls.io/repos/github/RobertDober/peg_to_parse/badge.svg?branch=master)](https://coveralls.io/github/RobertDober/peg_to_parse?branch=master)
+  [![Hex.pm](https://img.shields.io/hexpm/v/peg_to_parse.svg)](https://hex.pm/packages/peg_to_parse)
+  [![Hex.pm](https://img.shields.io/hexpm/dw/peg_to_parse.svg)](https://hex.pm/packages/peg_to_parse)
+  [![Hex.pm](https://img.shields.io/hexpm/dt/peg_to_parse.svg)](https://hex.pm/packages/peg_to_parse)
+    Documentation for `PegToParse`.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> PegToParse.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
+#  SPDX-License-Identifier: Apache-2.0
--- mix.exs
+++ mix.exs
@@ -1,13 +1,26 @@
 defmodule PegToParse.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @url "https://github.com/RobertDober/peg_to_parse"
+
   def project do
     [
       app: :peg_to_parse,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.10",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
+      test_coverage: [tool: ExCoveralls],
+      aliases: [docs: &build_docs/1]
     ]
   end
 
@@ -23,6 +36,61 @@ defmodule PegToParse.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    {:dialyxir, "~> 1.1.0", only: [:dev, :test], runtime: false},
+    {:excoveralls, "~> 0.14.4", only: [:test]},
+    {:extractly, "~> 0.5.3", only: [:dev]},
     ]
   end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "RELEASE.md",
+        "LICENSE"
+      ],
+      maintainers: [
+        "Robert Dober <robert.dober@gmail.com>"
+      ],
+      licenses: [
+        "Apache-2.0"
+      ],
+      links: %{
+        "Changelog" => "#{@url}/blob/master/RELEASE.md",
+        "GitHub" => @url
+      }
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support", "dev"]
+  defp elixirc_paths(:dev), do: ["lib", "bench", "dev"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  @prerequisites """
+  run `mix escript.install hex ex_doc` and adjust `PATH` accordingly
+  """
+  @module "EarmarkParser"
+  defp build_docs(_) do
+    Mix.Task.run("compile")
+    ex_doc = Path.join(Mix.path_for(:escripts), "ex_doc")
+    Mix.shell().info("Using escript: #{ex_doc} to build the docs")
+
+    unless File.exists?(ex_doc) do
+      raise "cannot build docs because escript for ex_doc is not installed, make sure to \n#{
+              @prerequisites
+            }"
+    end
+
+    args = [@module, @version, Mix.Project.compile_path()]
+    opts = ~w[--main #{@module} --source-ref v#{@version} --source-url #{@url}]
+
+    Mix.shell().info("Running: #{ex_doc} #{inspect(args ++ opts)}")
+    System.cmd(ex_doc, args ++ opts)
+    Mix.shell().info("Docs built successfully")
+  end
+
 end
+
+# SPDX-License-Identifier: Apache-2.0
