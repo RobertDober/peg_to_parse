9c9f3934495fac8484458ad561941c96a034a989
RobertDober <robert.dober@gmail.com>
2021-11-16 17:37:28 +0100

    Fixed bad branch in coverage badge (master -> main)

--- lib/peg_to_parse.ex
+++ lib/peg_to_parse.ex
@@ -2,7 +2,7 @@ defmodule PegToParse do
   @moduledoc """
 
   [![CI](https://github.com/robertdober/peg_to_parse/workflows/CI/badge.svg)](https://github.com/robertdober/peg_to_parse/actions)
-  [![Coverage Status](https://coveralls.io/repos/github/RobertDober/peg_to_parse/badge.svg?branch=master)](https://coveralls.io/github/RobertDober/peg_to_parse?branch=master)
+  [![Coverage Status](https://coveralls.io/repos/github/RobertDober/peg_to_parse/badge.svg?branch=main)](https://coveralls.io/github/RobertDober/peg_to_parse?branch=main)
   [![Hex.pm](https://img.shields.io/hexpm/v/peg_to_parse.svg)](https://hex.pm/packages/peg_to_parse)
   [![Hex.pm](https://img.shields.io/hexpm/dw/peg_to_parse.svg)](https://hex.pm/packages/peg_to_parse)
   [![Hex.pm](https://img.shields.io/hexpm/dt/peg_to_parse.svg)](https://hex.pm/packages/peg_to_parse)
